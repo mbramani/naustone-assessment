bfb2359b844f7b4a3a909cada8758bc28a78e761
Mohit Ramani <ramanimohit01@gmail.com>
2023-03-23 14:17:18 +0530

    updatre App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
+import { fetchUsers } from './features/users/usersActions'
+import { useDispatch } from 'react-redux'
+import TableHead from './components/table/TableHead'
+import TableBody from './components/table/TableBody'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch()
+  const columns = [
+    'SR No.',
+    'Account No.',
+    'Country',
+    'Division',
+    'Legacy No.',
+    'Legacy Division',
+    'Edit',
+  ]
+
+  useEffect(() => {
+    dispatch(fetchUsers())
+  }, [])
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div class="flex flex-col">
+      <div class="overflow-x-auto sm:-mx-6 lg:-mx-8 border rounded-lg shadow">
+        <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+          <div class="overflow-hidden">
+            <table className="min-w-full text-left text-sm font-light">
+              <TableHead columns={columns} />
+              <TableBody />
+            </table>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
